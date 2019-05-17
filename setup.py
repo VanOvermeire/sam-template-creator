fccc20e934f4aa0d898720a80553e3ec48a7ce45
Sam Van Overmeire <svanovermeire@branch.be>
2019-05-17 17:29:38 +0200

    Upgrade version to 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='sam-template-creator',
-      version='0.0.1',
+      version='0.1.0',
       description='Creates a SAM template for a given project',
       long_description=long_description,
       url='https://github.com/VanOvermeire/sam-template-creator',
