70784a75274d883bb6ac4eeea497e423ba2f20d9
Sam Van Overmeire <svanovermeire@branch.be>
2019-05-19 17:23:08 +0200

    Start with checking all dirs, not just under root

--- template_creator/reader/directory_scanner.py
+++ template_creator/reader/directory_scanner.py
@@ -8,6 +8,10 @@ from template_creator.reader.strategies import language_strategy_builder
 from template_creator.reader.FileInfo import FileInfo
 
 
+def find_all_non_hidden_dirs(location):
+    return list([x for x in Path(location).iterdir() if x.is_dir() and not x.name.startswith('.')])
+
+
 def get_number_of_files_for(language_suffix: str, file_names: List[str]):
     return len(list(x for x in file_names if x.endswith(language_suffix)))
 
@@ -48,27 +52,39 @@ def find_invoked_files(dirs, handler_file_lines, strategy, language_suffix) -> l
 
 
 # TODO might be better to explore deeper dirs (not just those under root), especially for Java (and Node)...
-# TODO and do we need to limit lambdas to their own dir? Might be advised though
+# TODO and do we need to limit lambdas to their own dir? Might be advised though...
 def find_lambda_files_in_directory(location: str, language: str) -> List[dict]:
     lambdas = []
-    dirs = list([x for x in Path(location).iterdir() if x.is_dir()])
 
+    root_dirs = find_all_non_hidden_dirs(location)
+    dirs = root_dirs.copy()
     language_suffix = LANGUAGES_WITH_SUFFIXES[language]
 
     for a_dir in dirs:
-        logging.debug('Checking dir {}'.format(a_dir.name))
-        for file in list(a_dir.glob('*{}'.format(language_suffix))):
-            logging.debug('Checking file {}'.format(file.name))
-            with file.open() as opened_file:
-                lines = opened_file.readlines()
-                true, handler_line = language_strategy_builder.is_handler_file_for(language, lines)
+        lambdas.extend(find_lambdas_in_subdir(location, language, language_suffix, a_dir, root_dirs))
+        dirs.extend(find_all_non_hidden_dirs(a_dir))
 
-                if true:
-                    logging.debug('Found handler file {} in dir {}'.format(file.name, a_dir.name))
-                    strategy = language_strategy_builder.build_strategy(language)
-                    executable = executables_in_dir(a_dir, strategy)
-                    other_file_lines = find_invoked_files(dirs, lines, strategy, language_suffix)
-                    file_info = FileInfo(location, a_dir, file, handler_line, lines, strategy, other_file_lines, executable)
-                    logging.debug('Building file info {} and adding to array'.format(file_info))
-                    lambdas.append(file_info.build())
+    return lambdas
+
+
+def find_lambdas_in_subdir(location: str, language: str, language_suffix: str, a_dir, root_dirs) -> List[dict]:
+    lambdas = []
+
+    logging.debug('Checking dir {}'.format(a_dir.name))
+
+    for file in list(a_dir.glob('*{}'.format(language_suffix))):
+        logging.debug('Checking file {}'.format(file.name))
+        with file.open() as opened_file:
+            lines = opened_file.readlines()
+            true, handler_line = language_strategy_builder.is_handler_file_for(language, lines)
+
+            if true:
+                logging.debug('Found handler file {} in dir {}'.format(file.name, a_dir.name))
+                strategy = language_strategy_builder.build_strategy(language)
+                executable = executables_in_dir(a_dir, strategy)
+                other_file_lines = find_invoked_files(root_dirs, lines, strategy, language_suffix)
+                file_info = FileInfo(location, a_dir, file, handler_line, lines, strategy, other_file_lines, executable)
+                logging.debug('Building file info {} and adding to array'.format(file_info))
+                lambdas.append(file_info.build())
+
     return lambdas
