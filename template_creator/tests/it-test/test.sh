f9e8d72d334dbc772297e2a0019d31df0e65e470
Sam Van Overmeire <svanovermeire@branch.be>
2019-06-03 19:05:06 +0200

    Add some checks to the it test

--- template_creator/tests/it-test/test.sh
+++ template_creator/tests/it-test/test.sh
@@ -39,6 +39,24 @@ aws cloudformation deploy --template-file outputSamTemplate.yaml --stack-name ${
 
 checkStatus "deploying"
 
+echo "Stack contains:"
+
+aws cloudformation describe-stack-resources --stack-name ${STACK_NAME} --query StackResources
+
+lambdas=$(aws cloudformation describe-stack-resources --stack-name arn:aws:cloudformation:eu-west-1:262438358359:stack/sam-creator-it-test/cc6c3830-856b-11e9-9ab0-0abc187d3320 --query 'StackResources[?ResourceType==`AWS::Lambda::Function`]' | jq length)
+apis=$(aws cloudformation describe-stack-resources --stack-name arn:aws:cloudformation:eu-west-1:262438358359:stack/sam-creator-it-test/cc6c3830-856b-11e9-9ab0-0abc187d3320 --query 'StackResources[?ResourceType==`AWS::ApiGateway::RestApi`]' | jq length)
+
+
+if [[ ${lambdas} -ne 1 ]]; then
+    echo "Expected to find one lambda in results but got $lambdas."
+    exit 1
+elif [[ ${apis} -ne 1 ]]; then
+    echo "Expected to find one api gateway in results but got $apis."
+    exit 1
+else
+    echo "Stack created with a lambda and rest api"
+fi
+
 echo "Running cleanup of created files and stack"
 
 cleanup ${STACK_NAME}
